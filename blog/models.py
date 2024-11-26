684c718a39d7ceac53694d239372371bb14ce746
Alireza Boostan <boostanalireza1999@gmail.com>
2024-11-26 09:46:11 +0330

    Create model classes Category, Post and Comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+ 
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    # author = models.ForeignKey()
+    created_at = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    placed_at = models.DateTimeField(auto_now_add=True)
+    
