2048149d6afcb4ba22c6a6bb56c10fad8827d154
Alireza Boostan <boostanalireza1999@gmail.com>
2024-12-07 19:17:07 +0330

    Add ProfileViewSet to urls

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register('posts', views.PostViewSet)
 router.register('categories', views.CategoryViewSet, basename='categories')
+router.register('profiles', views.ProfileViewSet)
 
 posts_router = routers.NestedDefaultRouter(router, 'posts', lookup='post')
 posts_router.register('comments', views.CommentViewSet, basename='post-comments')
