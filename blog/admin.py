1e64b6e394bff60c941c3ffc57e84b4c881b5dd3
Alireza Boostan <boostanalireza1999@gmail.com>
2024-12-08 21:11:22 +0330

    Add PostImageTabularInline

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from . import models
 
+
+class PostImageInline(admin.TabularInline):
+    model = models.PostImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != '':
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail" />')
+        return ''
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
@@ -8,8 +18,12 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ['title']
     }
+    inlines = [PostImageInline]
 
-
+    class Media:
+        css = {
+            'all': ['blog/styles.css']
+        }
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title']
