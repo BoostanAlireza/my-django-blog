f04bab97867d03102407f3089205b7e03fb2bda8
Alireza Boostan <boostanalireza1999@gmail.com>
2025-01-30 22:59:27 +0330

    Add custom permission classes: IsAdminOrAuthenticated, IsAuthorOrAdmin

--- blog/permissions.py
+++ blog/permissions.py
@@ -1,8 +1,26 @@
 from rest_framework import permissions
 
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_satff)
+
+class IsAdminOrAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated)
+
+class IsAuthorOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(obj.author == request.user or request.user.is_staff)
+    
+
+# class IsCommenter(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         return obj.commenter == request.user.username
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import permissions
@@ -8,7 +12,7 @@ from .models import Post, Category, Comment, PostImage, Author
 from .serializers import PostSerializer, CategorySerializer, CommentSerializer,  PostImageSerializer, AuthorSerializer
 from .filters import PostFilter
 from .pagination import DefaultPagination
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrAuthenticated, IsAuthorOrAdmin, IsAdminOrReadOnly
 
 
 class PostViewSet(ModelViewSet):
@@ -17,16 +21,19 @@ class PostViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     pagination_class = DefaultPagination
     filterset_class = PostFilter
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrAuthenticated, IsAuthorOrAdmin]
     search_fields = ['title', 'author']
     ordering_fields = ['created_at']
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
+    
 class PostImageViewSet(ModelViewSet):
     serializer_class = PostImageSerializer
 
     def get_serializer_context(self):
-        return {'post_id': self.kwargs['post_pk']}
+        return {'post_pk': self.kwargs['post_pk']}
 
     #This is for getting the specific image that we want to display and not all images
     def get_queryset(self):
@@ -46,11 +53,33 @@ class CategoryViewSet(ModelViewSet):
 
 
 class CommentViewSet(ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsCommenter]
 
+    def get_queryset(self):
+        post_pk = self.kwargs['post_pk']
+        return Comment.objects.filter(post_id=post_pk).all()
+
+    def get_serializer_context(self):
+        return {'post_pk': self.kwargs['post_pk']}
+    
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdminUser] #Only admin users can access all ations
+
+    #Each authenticated user should have access to his/her profile
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        user_id = request.user.id
+        author = Author.objects.get(user_id=user_id)
+        if request.method == 'GET':
+            serializer = AuthorSerializer(author)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = AuthorSerializer(author, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
